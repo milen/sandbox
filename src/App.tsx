a3414e6e8836f3f3819cce3eba1f1ee10dfab974
Milen Georgiev <m.v.georgiev@gmail.com>
2025-10-22 11:13:32 +0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React" />
         </a>
       </div>
       <h1>Vite / React</h1>
@@ -25,9 +25,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
